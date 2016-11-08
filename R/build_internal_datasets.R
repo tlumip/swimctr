8f3d4289798ba010aed355e3582903ea84cb284e
Rick Donnelly <rick.donnelly@gmail.com>
2016-11-08 09:23:05 -0700

    Commented build_internal_datasets, which was causing build problems, then roxygenised and successfully built package

--- R/build_internal_datasets.R
+++ R/build_internal_datasets.R
@@ -3,30 +3,30 @@
 # they fork and rebuild the package. It is assumed that the program is run from
 # the R folder, and will find the individual data files in the data-raw
 # directory, as suggested in Wickham's "R Packages" book.
-library(stringr)
+#library(stringr)
 
 # Start by getting a listing of files in the data-raw folder, which we'll read
 # into our environment.
-contents <- list.files(path = "../data-raw")
+#contents <- list.files(path = "../data-raw")
 
 # We'll read each file in turn
-for (this_file in contents) {
+#for (this_file in contents) {
   # Start by extracting the filename, which we'll assume is everything up to,
   # but not including, the final period
-  fullname <- strsplit(this_file, "\\.")[[1]]
-  filename <- fullname[1]
-  extension <- fullname[2]
-  if (extension != "csv") {
-    print(paste("Skipping processing of", fullname), quote = FALSE)
-    next
-  }
-  
+#  fullname <- strsplit(this_file, "\\.")[[1]]
+#  filename <- fullname[1]
+#  extension <- fullname[2]
+#  if (extension != "csv") {
+#    print(paste("Skipping processing of", fullname), quote = FALSE)
+#    next
+#  }
+
   # Read the file into a tibble
-  assign(filename, readr::read_csv(file.path("../data-raw", this_file)))
-}
+#  assign(filename, readr::read_csv(file.path("../data-raw", this_file)))
+#}
 
 # Remove the temporary variables we created above
-rm(list = c("contents", "extension", "filename", "fullname", "this_file"))
+#rm(list = c("contents", "extension", "filename", "fullname", "this_file"))
 
 # And finally, save the results in the data folder
-save.image(file = "../data/data.RData")
+#save.image(file = "../data/data.RData")
